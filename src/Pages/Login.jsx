7de826eefd1ee2674e2280ec7565d55ec9e16787
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-09-03 15:22:16 +0000

    Add image to login form

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,13 +1,17 @@
 import logo from '/narrowlite.svg';
-
+import backgroundImg from '/loginbg.png'; 
 
 function Login() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-2">
-      <div className="bg-white p-8  shadow-md rounded-lg w-96">
+    <div className="min-h-screen p-2 flex items-center justify-center bg-gray-100">
+      {/* Image on the Side */}
+      <div className="hidden md:block md:w-1/2 h-screen overflow-hidden">
+        <img src={backgroundImg} alt="Background" className="object-cover h-full w-full" />
+      </div>
+      <div className="bg-white px-4 py-7 shadow-md rounded-lg w-96">
         <div className="text-center mb-6">
           <img src={logo} alt="Company Logo" className="w-16 mx-auto" />
-          <h1 className="text-2xl font-semibold">NarrowLite</h1>
+          <h1 className="text-2xl font-semibold">NarrowLite International</h1>
         </div>
         <form>
           <div className="mb-4">
@@ -15,7 +19,7 @@ function Login() {
             <input
               id='username'
               type="text"
-              className="input input-bordered input-info w-full max-w-xs"
+              className="input input-bordered input-info w-full"
               placeholder="Enter your username"
             />
           </div>
@@ -24,14 +28,11 @@ function Login() {
             <input
               id='password'
               type="password"
-              className="input input-bordered input-info w-full max-w-xs"
+              className="input input-bordered input-info w-full"
               placeholder="Enter your password"
             />
           </div>
-          <button
-            type="submit"
-            className="btn btn-primary w-full mt-3"
-          >
+          <button type="submit" className="btn btn-primary w-full mt-3">
             Log In
           </button>
         </form>
