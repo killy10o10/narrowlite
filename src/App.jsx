0bb5119e27e30fb6fb29d188a89e8a544786ac17
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-09-03 17:08:18 +0000

    Setup react-router-dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import Login from "./Pages/Login";
+import { 
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route
+
+} from "react-router-dom"
+import Layout from "./components/Layout";
+import Page404 from "./Pages/Page404";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Login />} />
+      <Route path="*" element={<Page404 />} />
+    </Route>
+  )
+)
 
 function App() {
   return (
     <>
-      <Login />
+      <RouterProvider router={router} />
     </>
   );
 }
