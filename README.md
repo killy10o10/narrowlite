5e13faba43a2dc738295920a8cd470d6dc79f636
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-09-03 12:33:01 +0000

    Initial commit ⚙

---
+++ README.md
@@ -0,0 +1,1 @@
+# NarrowLite WebApp
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center">hello NarrowLite</h1>
+      <button className="btn btn-disabled">Click Me</button>
+    </>
+  );
+}
+
+export default App;
