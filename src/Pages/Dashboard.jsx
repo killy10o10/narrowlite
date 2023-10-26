ae86bc8662aae8c24b3ff77b97c72654f7f97054
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-10-26 01:49:27 +0000

    dashboard wip

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import { Avatar, Button } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
+import { MdOutlineSendToMobile } from "react-icons/md"
+import { BiPurchaseTag } from "react-icons/bi"
+import { FaSms } from "react-icons/fa"
+import { BsBookmarksFill } from "react-icons/bs"
 
 function Dashboard() {
   const location = useLocation();
   const { state } = location;
-
-  console.log(state)
 
   const [showToast, setShowToast] = useState(false);
 
@@ -29,30 +32,22 @@ function Dashboard() {
 
   return (
     <>
-      <div className="min-h-screen p-2 flex items-center justify-center bg-gray-100">
+      <div className="min-h-screen p-2 flex flex-col items-center bg-gray-100">
         <ToastContainer />
-        <div className="bg-white p-8 shadow-md rounded-lg w-96">
-          <div className="text-center mb-6">
-            <h1 className="text-2xl font-semibold">Two-Factor Authentication</h1>
-          </div>
-          <form>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">SMS Code</label>
-              <input
-                type="text"
-                className="input input-bordered input-info w-full"
-                placeholder="Enter the code from your SMS"
-              />
-            </div>
-            <button
-              type="submit"
-              className="btn btn-primary w-full">
-              Verify
-            </button>
-          </form>
+        <div className='flex gap-2 items-center self-center mt-10'>
+          <Avatar rounded />
+          <span className='font-bold text-2xl text-black'>{state.username}</span>
+        </div>
+
+        <div className='grid md:grid-cols-3 gap-4 my-auto'>
+          <Button color="dark" size="xl"><BiPurchaseTag className='text-2xl mr-2'/> <p className='text-xl'>Purchase Subscriptions</p></Button>
+          <Button color="dark" size="xl" className="flex items-center"><MdOutlineSendToMobile className='text-2xl mr-2' /> <p className='text-xl'>Send Ticket</p></Button>
+          <Button color="dark" size="xl" className="flex items-center"><FaSms className='text-2xl mr-2' /> <p className='text-xl'>SMS Management</p></Button>
+          <Button color="dark" size="xl" className="flex items-center"><BsBookmarksFill className='text-2xl mr-2' /> <p className='text-xl'>Book a Staff</p></Button>
         </div>
       </div>
     </>
+
   );
 }
 
