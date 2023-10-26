bc8a5172d3f658ae97dadb068a6905fbaa69dec4
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-10-26 02:20:30 +0000

    dashboard wip 2

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -31,22 +31,47 @@ function Dashboard() {
   }, [showToast]);
 
   return (
-    <>
-      <div className="min-h-screen p-2 flex flex-col items-center bg-gray-100">
-        <ToastContainer />
-        <div className='flex gap-2 items-center self-center mt-10'>
-          <Avatar rounded />
-          <span className='font-bold text-2xl text-black'>{state.username}</span>
-        </div>
+<>
+  <div className="min-h-screen flex flex-col items-center bg-cover bg-center relative" style={{ backgroundImage: "url('/illustration.svg')" }}>
+    <div className="absolute inset-0 bg-black opacity-50"></div>
+    <ToastContainer />
+    <div className='flex gap-2 items-center self-center mt-10 z-10'>
+      <Avatar rounded />
+      <span className='font-bold text-2xl text-white'>{state.username}</span>
+    </div>
 
-        <div className='grid md:grid-cols-3 gap-4 my-auto'>
-          <Button color="dark" size="xl"><BiPurchaseTag className='text-2xl mr-2'/> <p className='text-xl'>Purchase Subscriptions</p></Button>
-          <Button color="dark" size="xl" className="flex items-center"><MdOutlineSendToMobile className='text-2xl mr-2' /> <p className='text-xl'>Send Ticket</p></Button>
-          <Button color="dark" size="xl" className="flex items-center"><FaSms className='text-2xl mr-2' /> <p className='text-xl'>SMS Management</p></Button>
-          <Button color="dark" size="xl" className="flex items-center"><BsBookmarksFill className='text-2xl mr-2' /> <p className='text-xl'>Book a Staff</p></Button>
-        </div>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 my-auto z-10'>
+      <div className="text-center">
+        <Button color="light" size="xl" className="hover:bg-blue-600 w-full hover:text-white transition-transform transform hover:scale-105">
+          <BiPurchaseTag className='text-2xl mr-2' />
+          <p className='text-xl'>Purchase Subscriptions</p>
+        </Button>
       </div>
-    </>
+
+      <div className="text-center">
+        <Button color="light" size="xl" className="hover:bg-green-600 w-full hover:text-white transition-transform transform hover:scale-105">
+          <MdOutlineSendToMobile className='text-2xl mr-2' />
+          <p className='text-xl'>Send Ticket</p>
+        </Button>
+      </div>
+
+      <div className="text-center">
+        <Button color="light" size="xl" className="hover:bg-yellow-600 w-full hover:text-white transition-transform transform hover:scale-105">
+          <FaSms className='text-2xl mr-2' />
+          <p className='text-xl'>SMS Management</p>
+        </Button>
+      </div>
+
+      <div className="text-center">
+        <Button color="light" size="xl" className="hover:bg-indigo-600 w-full hover:text-white transition-transform transform hover:scale-105">
+          <BsBookmarksFill className='text-2xl mr-2' />
+          <p className='text-xl'>Book a Staff</p>
+        </Button>
+      </div>
+    </div>
+  </div>
+</>
+
 
   );
 }
